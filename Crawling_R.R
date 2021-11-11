4c2ce807217822c43565338fe10ca1dd314c4b76
Dakyeom <63940054+dakyeoman@users.noreply.github.com>
2021-11-11 12:27:40 +0900

    Add files via upload

--- Crawling_R.R
+++ Crawling_R.R
@@ -37,7 +37,7 @@ tail(m3)
 
 m2
 m4
- m4 <- NULL
+m4 <- NULL
 for(i in 1:10){
   m4 <- rbind(m4, m2) 
   cat("\n", i) #"\n" = enter
@@ -54,14 +54,14 @@ m3[str_detect(m3$v6, "odd"),]
 
 m3[m3$v6 %in% "odd", ]
 
-setwd("/Users/adrua/Desktop/Crawling_R")
+setwd("/Users/adrua/Desktop/R-Crawling_new/R-Crawling/")
 getwd()  
 
 write.csv(m3, "m3.csv", row.names=F) #row.names=F: 행 이름 저장X
 m4 <- read.csv("m3.csv")
 m4
 
-setwd("/Users/adrua/Desktop/Crawling_R")
+setwd("/Users/adrua/Desktop/R-Crawling_new/R-Crawling/")
 file_list <- list.files()
 
 library(stringr)
@@ -75,7 +75,7 @@ final_data <- NULL
 for(i in 1:length(folder_list)){
   
   folder_list[i]
-  setwd(paste0("/Users/adrua/Desktop/Crawling_R/",folder_list[i]))
+  setwd(paste0("/Users/adrua/Desktop/R-Crawling_new/R-Crawling/",folder_list[i]))
   getwd()
   file_list <- list.files()
   
@@ -94,7 +94,7 @@ str(final_data)
 
 
 #roof2
-setwd("/Users/adrua/Desktop/R-Crawling/")
+setwd("/Users/adrua/Desktop/R-Crawling_new/R-Crawling/")
 
 data <- read.csv("m3.csv")
 head(data)
@@ -114,16 +114,16 @@ final_data1 <- NULL
 final_data2 <- NULL
 
 for(i in 1:length(folder_list)){
- 
+  
   folder_list[i]
-  setwd(paste0("/Users/adrua/Desktop/R-Crawling/",folder_list[i]))
+  setwd(paste0("/Users/adrua/Desktop/R-Crawling_new/R-Crawling/",folder_list[i]))
   getwd()
   file_list <- list.files()
   
   j <- 2
   for(j in 1:length(file_list)){
-   
-     data <- read.csv(file_list[j])
+    
+    data <- read.csv(file_list[j])
     if(ncol(data)==6){
       final_data1 <- rbind(final_data1, data)
       #col수가 6개면 final data로 rbind 시킴. 
@@ -190,7 +190,7 @@ lapply(aa, first)
 
 index <- function(x, i) { 
   x[i] #i번째 방 출력 
-  }
+}
 index(aa[[1]], 2)
 sapply(aa, index, 3)
 
@@ -200,56 +200,62 @@ dd <- c() #빈 벡터 선언
 for(i in 1: length(aa)){ 
   dd <- c(aa[[i]][1])
   cat("\n", i)
-  }
+}
 
 
 library(stringr)
 ##클리앙 크롤링
 i <- 1
-for(i in 1:10){
-url <- paste0("https://www.clien.net/service/board/park?&od=T31&category=0&po=0", i= -1)
-b <- readLines(url, encoding = "UTF-8") ##or EUC-KR
-length(b)
-head(b, 30)
-tail(b)
-
-#str_extract(b2, "(?<=abc").*(?=ijk)")##abc와 ijk 사이에 있는 텍스트 추출 
-#str_extract(data, "(?<=abc).*(?=)")##앞에 abc가 있는 것 추출
-#str_extract(data, "(?<=).*(?=i)")##뒤에 i가 있는 것 추출
-
-#제목 추출
-b2<- b[str_detect(b, "subject_fixed")]
-title <- str_extract(b2, "(?<=title=\").*(?=\">)")
-
-
-#조회수 추출 
-b3<- b[str_detect(b, "<span class=\"hit\">")];b3
-b4 <- str_split(b3, "hit\">");b4
-##way1
-
-for(i in 1:10){
-hit <- str_extract(b3, "(?<=\">).*(?=</span>)")[-1]
-#sapply(b4, function(x){x[1]}) #함수 바로 작성 가능 
-
-##way2
-#ff<- function(x) {
-#   x[2]
-# }
-# sapply(b4, ff)
-# str_sub(sapply(b4, ff), end= -8) #end= -8: 뒤에서 글자 8개를 삭제 *변동 
-
-
-##url 추출
-b5 <- b[which(str_detect(b, "subject_fixed")) -2] 
-#which: TRUE값이 있는 index만 추출
-#-2 : 소스코드에서 제목 두 줄 앞 
-
-b6 <- str_sub(str_extract(b5, "(?<=href=\").*(?=data-role)"), end = -4)
-url <- paste0("https://www.clien.net", b6)
-
-data <- cbind(title, hit, url) 
-
+final_data <- NULL
+for(i in 0:9){
+  url <- paste0("https://www.clien.net/service/board/park?&od=T31&category=0&po=0", i)
+  b <- readLines(url, encoding = "UTF-8") ##or EUC-KR
+  length(b)
+  head(b, 30)
+  tail(b)
+  
+  #str_extract(b2, "(?<=abc").*(?=ijk)")##abc와 ijk 사이에 있는 텍스트 추출 
+  #str_extract(data, "(?<=abc).*(?=)")##앞에 abc가 있는 것 추출
+  #str_extract(data, "(?<=).*(?=i)")##뒤에 i가 있는 것 추출
+  
+  #제목 추출
+  b2<- b[str_detect(b, "subject_fixed")]
+  title <- str_extract(b2, "(?<=title=\").*(?=\">)");title
+  
+  
+  #조회수 추출 
+  b3<- b[str_detect(b, "<span class=\"hit\">")];b3
+  #b4 <- str_split(b3, "hit\">");b4
+  ##way1
+    hit <- str_extract(b3, "(?<=\">).*(?=</span>)")[-1]
+    #sapply(b4, function(x){x[1]}) #함수 바로 작성 가능 
+    
+    ##way2
+    #ff<- function(x) {
+    #   x[2]
+    # }
+    # sapply(b4, ff)
+    # str_sub(sapply(b4, ff), end= -8) #end= -8: 뒤에서 글자 8개를 삭제 *변동 
+    
+    
+    ##url 추출
+    b5 <- b[which(str_detect(b, "subject_fixed")) -2] 
+    #which: TRUE값이 있는 index만 추출
+    #-2 : 소스코드에서 제목 두 줄 앞 
+    
+    b6 <- str_sub(str_extract(b5, "(?<=href=\").*(?=data-role)"), end = -4)
+    url <- paste0("https://www.clien.net", b6)
+    
+    data <- cbind(title, hit, url) 
+    final_data <- rbind(final_data, data)
+    cat("\n", i)
 }
+length(title)
+dim(final_data)
+head(final_data)
+tail(final_data)
 
-head(data)
+setwd("/Users/adrua/Desktop/R-Crawling_new/R-Crawling")
+write.csv(final_data, "base_data.csv", row.names=F)
 
+ 
