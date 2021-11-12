8e0c97720f2515dbd55b8e0f129a2baeb8653485
An Dakyeom <63940054+dakyeoman@users.noreply.github.com>
2021-11-12 13:52:28 +0900

    CrawlDCinside2/2

--- DCinside_superidea.R
+++ DCinside_superidea.R
@@ -1,16 +1,17 @@
-
+library(stringr)
 dc_data <- NULL
 for(i in 1:10){ 
   url <- paste0("https://gall.dcinside.com/board/lists/?id=superidea&page=", i)
+  Sys.sleep(runif(1)*2)
   b <- readLines(url, encoding = "UTF-8")
-  library(stringr)
+  
   
   #제목/url
   index <- which(str_detect(b, "gall_tit ub-word"))[-1]
   b2 <- b[index+1]
   title <- str_trim(str_extract(b2, ("(?<=</strong>).*(?=</a>)")))
   con_url <- str_sub(str_extract(b2, ("(?<=a href=).*(?=em class)")), 3, end = -17)
-  con_url2 <- paste0("https://gall.dcinside.com/", con_url)
+  con_url2 <- paste0("https://gall.dcinside.com/", con_url) 
   
   
   #조회수
@@ -30,3 +31,42 @@ head(dc_data)
 dim(dc_data)
 setwd("/Users/adrua/Desktop/R-Crawling_new/R-Crawling/")
 write.csv(dc_data, "dc_data.csv", row.names = F)
+
+
+
+
+
+
+
+
+#(2)
+#gallview_contents<!--  본문 우측 광고 -->
+
+data <- read.csv("dc_data.csv")
+head(data)
+dim(data)
+
+url <- as.character(data$con_url2);url
+length(url)
+i <- 1
+contents <- c()
+for(i in 1:length(url)){
+  b <- readLines(url[i], encoding = "UTF-8")
+  
+  #Sys.sleep(runif(1)*2)
+  
+  b2 <- b[which(str_detect(b, "gallview_contents")):which(str_detect(b, " 본문 우측 광고 "))]
+  b3 <- paste(b2, collapse = " ");b3
+  b4 <- gsub("<.*?>", "", b3);b4
+  b5 <- gsub("\t|&nbsp;", "", b4) ; b5
+  con <- str_trim(b5);con
+  contents <- c(contents, con)
+  cat("\n", i)
+}
+head(contents)
+dim(data)
+dim(contents)
+
+data2 <- cbind(data, contents) ##에러
+write.csv(data2, "dc.csv", row.names = F) ##이거 아닌 것 같은데. 
+
